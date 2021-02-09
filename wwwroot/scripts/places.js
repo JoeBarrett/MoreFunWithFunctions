febdc3b99772d13340ec2947eb6dfd231c79c1d5
Joe Barrett <joebarrett.mct@outlook.com>
2021-02-09 10:55:42 -0800

    Add project files.

---
+++ wwwroot/scripts/places.js
@@ -0,0 +1,23 @@
+﻿class Place {
+    constructor(where,what) {
+        this.where = where;
+        this.what = what;
+    }
+}
+
+
+
+let _myplaces = [];
+    (function() {
+        _myplaces[0] = new Place("Greece", "The Parthenon");
+        _myplaces[1] = new Place("Cleveland", "Rock 'n' Roll Hall of Fame");
+        _myplaces[2] = new Place("Tennessee", "World's Largest Ball of Twine");
+        _myplaces[3] = new Place("North Carolina", "World's Second Largest Ball of Twine");
+        _myplaces[4] = new Place("Seattle", "Pike Place Market");
+    }) ();
+    
+
+
+
+
+
